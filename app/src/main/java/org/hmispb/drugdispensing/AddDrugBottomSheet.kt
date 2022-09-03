873cf10a72838b6103821226af89afd89016cc66
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-09-03 14:48:21 +0530

    added bottom sheet

---
+++ app/src/main/java/org/hmispb/drugdispensing/AddDrugBottomSheet.kt
@@ -0,0 +1,49 @@
+package org.hmispb.drugdispensing
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AddDrugBottomSheet : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottomsheet_add_drug_details, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val drugs = listOf("Drug 1", "Drug 2", "Drug 3")
+        val spinner = view.findViewById<Spinner>(R.id.spinner_drug_name)
+        if (spinner!=null){
+            val adapter = ArrayAdapter(requireContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                drugs
+            )
+            spinner.adapter = adapter
+            spinner.onItemSelectedListener = object :
+            AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    Toast.makeText(requireContext(), drugs[p2], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    TODO("Not yet implemented")
+                }
+
+            }
+        }
+    }
+}
